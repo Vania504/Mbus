4258d8958569d831a453a7fb2efcef6415625b8b
Andriy <77618488+AndriykoH@users.noreply.github.com>
2022-12-12 22:01:19 +0200

    added search field

--- src/components/forUser/Routes/routeDetailPage/routeDetailPage.vue
+++ src/components/forUser/Routes/routeDetailPage/routeDetailPage.vue
@@ -4,20 +4,62 @@
     <div v-else>
       <div v-if="$vuetify.breakpoint.xs">
         <v-col class="px-0 py-0">
-          <div style="background-color: #243949; align-self: center; height: 40px">
+          <div
+            style="background-color: #243949; align-self: center; height: 40px"
+          >
             <v-row justify="start" align="center" no-gutters class="pt-2">
-              <v-icon class="ml-2 mr-2" color="white" @click="$router.push($route.query.start_route ? `/routes?start_route=${$route.query.start_route}&end_route=${$route.query.end_route}` : '/routes')">mdi-arrow-left</v-icon>
-              <span style="color: white; font-size: 18px">{{ route.departure }}–{{ route.destination }}</span>
+              <v-icon
+                class="ml-2 mr-2"
+                color="white"
+                @click="
+                  $router.push(
+                    $route.query.start_route
+                      ? `/routes?start_route=${$route.query.start_route}&end_route=${$route.query.end_route}`
+                      : '/routes'
+                  )
+                "
+                >mdi-arrow-left</v-icon
+              >
+              <span style="color: white; font-size: 18px"
+                >{{ route.departure }}–{{ route.destination }}</span
+              >
             </v-row>
           </div>
+          <div
+            style="
+              width: 1440px;
+              height: 80px;
+              background: rgba(255, 255, 255, 0.9);
+              border-bottom: 0.5px solid #085895;
+              box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.15);
+              text-align: center;
+            "
+          ></div>
           <route-map :start_point="start_point" :finish_point="finish_point" />
           <route-description :route="route" />
           <detail-route-info :route="route" />
+          <searchRoutesField />
         </v-col>
       </div>
       <div v-else>
-        <routes-header :title="route.departure + '-' + route.destination"
-          :image="route.images.length > 0 ? route.images[0].images.path : ''" />
+        <routes-header
+          :title="route.departure + '-' + route.destination"
+          :image="route.images.length > 0 ? route.images[0].images.path : ''"
+        />
+        <div
+          style="
+            width: 1440px;
+            height: 80px;
+            background: rgba(255, 255, 255, 0.9);
+            border-bottom: 0.5px solid #085895;
+            box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.15);
+            text-align: center;
+          "
+        >
+          <v-row no-gutters justify="center" class="pt-3">
+            <div style="width: 804px"><search-routes-field /></div>
+          </v-row>
+        </div>
         <v-row justify="center" align="start" no-gutters class="mt-15">
           <route-map :start_point="start_point" :finish_point="finish_point" />
           <route-description :route="route" />
@@ -37,7 +79,8 @@ import routeDescription from "./routeDescription.vue";
 import detailRouteInfo from "./detailRouteInfo.vue";
 import routesService from "@/requests/admin/routesService";
 import Loader from "@/components/UI/Loader.vue";
-import googleMapsService from "@/requests/googleMaps/googleMapsService"
+import googleMapsService from "@/requests/googleMaps/googleMapsService";
+import searchRoutesField from "@/components/UI/searchRoutesField.vue";
 export default {
   components: {
     routesHeader,
@@ -45,12 +88,13 @@ export default {
     routeDescription,
     detailRouteInfo,
     Loader,
+    searchRoutesField,
   },
   data: () => ({
     route: {},
     showLoader: true,
     start_point: {},
-    finish_point: {}
+    finish_point: {},
   }),
   mounted() {
     this.getRoute();
@@ -65,7 +109,7 @@ export default {
       this.showLoader = false;
     },
     async getPoint(city) {
-      let response = await googleMapsService.getCoordinates(city)
+      let response = await googleMapsService.getCoordinates(city);
       return response.results[0].geometry.location;
     },
   },
@@ -73,5 +117,4 @@ export default {
 </script>
 
 <style>
-
 </style>
